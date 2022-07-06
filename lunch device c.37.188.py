31d0f16626fa33a9ca534f0832d0fc04ced0f8bc
s.danilin <danilinekb@gmail.com>
2022-07-06 17:51:05 +0300

    v.2

--- lunch device c.37.188.py
+++ lunch device c.37.188.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch()
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
     context = browser.new_context()
     # Open new page
     page = context.new_page()
